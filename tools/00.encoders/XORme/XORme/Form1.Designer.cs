80366584bb2be03f9edfa871690051f735be50a5
blu3drag0nsec@gmail.com <blu3drag0nsec@gmail.com>
2025-08-06 16:39:04 -0400

    chore: adding process hollowing and xorme app

---
+++ tools/00.encoders/XORme/XORme/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace XORme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txSource = new System.Windows.Forms.TextBox();
+            this.txDestination = new System.Windows.Forms.TextBox();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnV = new System.Windows.Forms.Button();
+            this.txXORKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txSource
+            // 
+            this.txSource.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txSource.Location = new System.Drawing.Point(0, 0);
+            this.txSource.Multiline = true;
+            this.txSource.Name = "txSource";
+            this.txSource.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txSource.Size = new System.Drawing.Size(504, 585);
+            this.txSource.TabIndex = 0;
+            // 
+            // txDestination
+            // 
+            this.txDestination.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txDestination.Location = new System.Drawing.Point(593, 0);
+            this.txDestination.Multiline = true;
+            this.txDestination.Name = "txDestination";
+            this.txDestination.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txDestination.Size = new System.Drawing.Size(504, 585);
+            this.txDestination.TabIndex = 1;
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(510, 148);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(75, 23);
+            this.btnC.TabIndex = 2;
+            this.btnC.Text = "C#";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnV
+            // 
+            this.btnV.Location = new System.Drawing.Point(510, 201);
+            this.btnV.Name = "btnV";
+            this.btnV.Size = new System.Drawing.Size(75, 23);
+            this.btnV.TabIndex = 3;
+            this.btnV.Text = "VBA";
+            this.btnV.UseVisualStyleBackColor = true;
+            this.btnV.Click += new System.EventHandler(this.btnV_Click);
+            // 
+            // txXORKey
+            // 
+            this.txXORKey.Location = new System.Drawing.Point(510, 90);
+            this.txXORKey.Name = "txXORKey";
+            this.txXORKey.Size = new System.Drawing.Size(77, 20);
+            this.txXORKey.TabIndex = 4;
+            this.txXORKey.Text = "b3 1b fa ee";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 585);
+            this.Controls.Add(this.txXORKey);
+            this.Controls.Add(this.btnV);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.txDestination);
+            this.Controls.Add(this.txSource);
+            this.Name = "Form1";
+            this.Text = "XORme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txSource;
+        private System.Windows.Forms.TextBox txDestination;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnV;
+        private System.Windows.Forms.TextBox txXORKey;
+    }
+}
+
---
+++ tools/00.encoders/XORme/XORme/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XORme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] ParseInput(string input)
+        {
+            var cleaned = input.Replace("byte[] buf =", "")
+                               .Replace("{", "")
+                               .Replace("}", "")
+                               .Replace(";", "")
+                               .Replace("\n", "")
+                               .Replace("\r", "")
+                               .Trim();
+            int newByteIndex = cleaned.IndexOf("new byte[");
+            if (newByteIndex != -1)
+            {
+                int closingBracket = cleaned.IndexOf(']', newByteIndex);
+                if (closingBracket != -1)
+                {
+                    cleaned = cleaned.Remove(newByteIndex, (closingBracket - newByteIndex) + 1);
+                }
+            }
+
+            var bytes = cleaned.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return bytes.Select(b => Convert.ToByte(b.Replace("0x", ""), 16)).ToArray();
+        }
+
+        private string FormatCSharp(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"byte[] buf = new byte[{data.Length}] {{");
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.AppendFormat("0x{0:x2}", data[i]);
+                if (i != data.Length - 1) sb.Append(", ");
+                if ((i + 1) % 15 == 0) sb.AppendLine();
+            }
+            sb.AppendLine("};");
+            return sb.ToString();
+        }
+
+        private string FormatVBA(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Dim buf As Variant");
+            int counter = 0;
+            foreach (var b in data)
+            {
+                sb.Append($"{b}, ");
+                counter++;
+                if (counter % 25 == 0) sb.AppendLine("_");
+            }
+            return sb.ToString();
+        }
+        private byte[] ParseKey(string keyInput)
+        {
+            if (string.IsNullOrWhiteSpace(keyInput))
+                throw new ArgumentException("Key cannot be empty.");
+
+            var bytes = keyInput.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            return bytes.Select(b => Convert.ToByte(b, 16)).ToArray();
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] inputBytes = ParseInput(txSource.Text);                
+                byte[] keyBytes = ParseKey(txXORKey.Text);
+
+                byte[] result = new byte[inputBytes.Length];
+                for (int i = 0; i < inputBytes.Length; i++)
+                {
+                    result[i] = (byte)(inputBytes[i] ^ keyBytes[i % keyBytes.Length]);
+                }
+
+                txDestination.Text = FormatCSharp(result);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void btnV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] inputBytes = ParseInput(txSource.Text);
+                byte[] keyBytes = ParseKey(txXORKey.Text);
+
+                byte[] result = new byte[inputBytes.Length];
+                for (int i = 0; i < inputBytes.Length; i++)
+                {
+                    result[i] = (byte)(inputBytes[i] ^ keyBytes[i % keyBytes.Length]);
+                }
+
+                txDestination.Text = FormatVBA(result);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
